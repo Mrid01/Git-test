12086045b117c74a6d8c4aa4d02305532b56bfae
root <root@ip-172-31-5-200.ap-south-1.compute.internal>
2023-06-20 05:26:00 +0000

    Addition Feature added.

---
+++ calculator.py
@@ -0,0 +1,8 @@
+print("hello git!")
+print("To use this calculator please input two numbers after the prompt:")
+# Prompt for user input
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+sum = a+b;
+print(sum);
+print("Thank you")
