ad751f818a508c9c893cc670eb0b4be7347521f7
root <root@ip-172-31-5-200.ap-south-1.compute.internal>
2023-06-20 05:45:04 +0000

    Multiplication Feature added.

--- calculator.py
+++ calculator.py
@@ -5,6 +5,8 @@ a = float(input("Enter the first number: "))
 b = float(input("Enter the second number: "))
 add = a+b;
 sub = a-b;
+multiply = a*b;
 print('Sum : ',add);
-print("Difference : ",sub)
+print("Difference : ",sub);
+print("Product : ",multiply);
 print("Operation Successful! Exiting...")
